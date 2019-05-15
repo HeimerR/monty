bf45c8455d753a98ea413d7cf7908662dbf88d98
Heimer <732@holbertonschool.com>
2019-05-15 23:53:11 +0000

    task 4 add added

--- execute.c
+++ execute.c
@@ -15,6 +15,7 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 				{"pint", f_pint},
 				{"pop", f_pop},
 				{"swap", f_swap},
+				{"add", f_add},
 				{"nop", f_nop},
 				{NULL, NULL}
 				};
--- monty.h
+++ monty.h
@@ -59,5 +59,6 @@ int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
 void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
 void f_nop(stack_t **head, unsigned int counter);
 #endif
