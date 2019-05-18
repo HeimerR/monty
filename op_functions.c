822589961ab4762f6414e82dd9e76d06cd554a47
Alexa <alexafdeveloper@gmail.com>
2019-05-18 14:54:40 +0000

    pall

--- op_functions.c
+++ op_functions.c
@@ -17,23 +17,3 @@ void f_pint(stack_t **head, unsigned int counter)
 	}
 	printf("%d\n", (*head)->n);
 }
-/**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
-
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-}
